2a0a8feb3fc83cd8f5403cf3cca59a793d663e62
juanmolina76 <molinajuanj@hotmail.com>
2024-05-12 16:38:42 -0300

    boton mostrar mas con js

--- js/historia.js
+++ js/historia.js
@@ -1,14 +1,18 @@
+/*  AL HACER CLICK SOBRE EL AÑO en la linea historica
+cambia el color y queda fijo..agregar mostrar el parrafo 
+oculto. POR ahora reeplazado por h3:hover en css*/
 document.querySelector("#cColor").addEventListener('onmouseover', funCambioColor)
 
 function funCambioColor() {
     const caColoranio = document.querySelectorAll("h3")
     console.log(caColoranio)
-caColoranio.style="color: red; background: blue;"
-
+caColoranio.style="color: yellow; background: blue;"
 }
-document.querySelector("historia").addEventListener('onmouseover', mostrarParrafo)
 
-const mostrarParrafo =() => {
-    const parrafo1 = document.querySelector("hisotira-5")
-    parrafo1.style.display = "none"
+/*  BOTON "VER MAS" muestra el parrafo 'porqué elegirnos'*/ */
+document.querySelector("#ver_mas").addEventListener('click',mostrarMas)
+
+function mostrarMas() {
+    const porQueElegir = document.querySelector("#k")
+    porQueElegir.style.display = "block"
 }
