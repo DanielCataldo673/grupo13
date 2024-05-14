164b1a90c13d18b14e15c345e2c82e1f8ccf186c
Dario Javier Correa <dario.javier.correa@emeal.nttdata.com>
2024-05-14 17:59:36 -0300

    Se agrega validacion campos en Contacto por Javascript

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,6 +1,32 @@
-const fnEnviar = () => {
-  let btnEnviar = document.getElementById("btnEnviar");
-  btnEnviar.click();
+const validarCampos = () =>{
+
+  var campos = document.getElementsByClassName("requerido");
+
+  for (let index = 0; index < campos.length; index++) {
+    const campo = campos[index];
+    
+    if (campo.value == ""){
+      campo.setCustomValidity("El campo es obligatorio");
+      campo.reportValidity();
+
+      return false;
+    }
+    else
+    {
+      campo.setCustomValidity("");
+      campo.reportValidity();
+    }
+  }
+  return true;
+}
+
+const fnEnviar = (e) => {
+  e.preventDefault();
+  if (validarCampos())
+  {
+    let btnEnviar = document.getElementById("btnEnviar");
+    btnEnviar.click();
+  }
 }
 
 // function fnEnviar(){
@@ -24,6 +50,7 @@ const fnSubirImagen = (e) => {
   }
 }
 
-document.getElementById("myForm").addEventListener("submit", fnEnviar);
+//document.getElementById("myForm").addEventListener("submit", fnEnviar);
+document.getElementById("enviarForm").addEventListener("click", fnEnviar);
 
 document.getElementById("imageInput").addEventListener("change", fnSubirImagen);
