031b8787a7af40251cfc5dcd978b30cc3241c5e3
DanielCataldo673 <danielcataldo673@gmail.com>
2024-05-15 18:25:11 -0300

    first commit

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,12 +1,48 @@
-const fnEnviar = () => {
-  let btnEnviar = document.getElementById("btnEnviar");
-  btnEnviar.click();
+const validarCampos = () => {
+  let campos = document.getElementsByClassName("requerido");
+
+  for (let index = 0; index < campos.length; index++) {
+    const campo = campos[index];
+    
+    if (campo.value == ""){
+      campo.setCustomValidity("El campo es obligatorio");
+      campo.reportValidity();
+
+      return false;
+    }
+    else
+    {
+      campo.setCustomValidity("");
+      campo.reportValidity();
+    }
+  }
+  return true;
 }
 
-// function fnEnviar(){
-//   let btnEnviar = document.getElementById("btnEnviar");
-//   btnEnviar.click();
-// }
+const limpiarCampos = () => {
+  let campos = document.getElementsByClassName("requerido");
+
+  for (let index = 0; index < campos.length; index++) {
+    const campo = campos[index];
+    campo.value = ""; // Limpiar el valor del campo
+  }
+
+  let camposElegir = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
+
+  camposElegir.forEach(function(campo) {
+    campo.checked = false; // Deseleccionar el checkbox o radio
+  });
+}
+
+const fnEnviar = (e) => {
+  e.preventDefault();
+  if (validarCampos())
+  {
+    let btnEnviar = document.getElementById("btnEnviar");
+    btnEnviar.click();
+    limpiarCampos(); // Llamar a la función para limpiar los campos
+  }
+}
 
 const fnSubirImagen = (e) => {
   let archivo = e.target.files[0];
@@ -24,6 +60,5 @@ const fnSubirImagen = (e) => {
   }
 }
 
-document.getElementById("myForm").addEventListener("submit", fnEnviar);
-
+document.getElementById("enviarForm").addEventListener("click", fnEnviar);
 document.getElementById("imageInput").addEventListener("change", fnSubirImagen);
