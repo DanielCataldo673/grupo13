8c005f36bf9b6a28f4708f7b46843de22813248e
DanielCataldo673 <danielcataldo673@gmail.com>
2024-06-19 13:30:10 -0300

    first commit

--- src/routes/mainRoutesProducto.js
+++ src/routes/mainRoutesProducto.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const controladores = require(`../controllers/mainControllerProducto`)
-//const path = require('path')
+const express = require('express');
+const router = express.Router();
+const controladores = require('../controllers/mainControllerProducto');
+//const path = require('path');
 
-router.get('/producto', controladores.getProducto)
-router.post('/producto', controladores.crearRegistro)
-router.get('/modificar/:id', controladores.getModificar)
-router.put('/modificar', controladores.actualizar)
-router.delete('/producto', controladores.eliminar)
+router.get('/producto', controladores.getProducto);
+router.post('/producto', controladores.crearRegistro);
+router.get('/modificar/:id', controladores.getModificar);
+router.put('/modificar', controladores.actualizar);
+router.delete('/producto', controladores.eliminar);
 
-module.exports = router
+module.exports = router;
