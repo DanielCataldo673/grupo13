287dd0486da7f9984455dbb7afc2f2b55a20080b
Darío Javier Correa <dcorrea@emeal.nttdata.com>
2024-06-24 16:24:35 -0300

    Cambios para que se llene desde la DB el combo Variedad

--- src/controllers/mainControllerProducto.js
+++ src/controllers/mainControllerProducto.js
@@ -3,9 +3,12 @@ const { conn } = require('../db/dbconnect');
 module.exports = {
     getProducto: async (req, res) => {
         try {
-            const [registros] = await conn.query(`SELECT * FROM producto`);
+            const [registros] = await conn.query(`SELECT p.*, v.nombre AS nombre_variedad FROM producto p 
+                                                    JOIN variedad v ON p.variedad_id = v.id`);
+
+            const [variedades] = await conn.query(`SELECT * FROM variedad`);
             // Aquí cambia `producto` por `productos` para asegurarte de que coincida con el nombre que estás utilizando en la vista
-            res.render('producto', { productos: registros, tituloDePagina: 'Listado de Productos' });
+            res.render('producto', { productos: registros, variedades: variedades, tituloDePagina: 'Listado de Productos' });
         } catch (error) {
             throw error;
         } finally {
@@ -21,18 +24,22 @@ module.exports = {
 
     getModificar: async (req, res) => {
         const [modificar] = await conn.query(`SELECT *, imagen FROM producto WHERE id=?`, [req.params.id]);
+        
+        const [variedades] = await conn.query(`SELECT * FROM variedad`);
         // Ya está definido correctamente en tu código.
         res.render('modificar', {
             tituloDePagina: 'Modificar Items',
             registro: modificar[0],
+            variedades: variedades,
             imagen: modificar[0].imagen // Asegúrate de que la imagen esté definida
         });
     },
 
     actualizar: async (req, res) => {
         const sql = `UPDATE producto SET nombre=?, caracteristicas=?, imagen=?, precio=?, gramaje=?, variedad_id=? WHERE id=?`;
-        const { idMod, nombre, caracteristicas, imagen, precio, gramaje, variedad_id } = req.body;
-        const modificado = await conn.query(sql, [nombre, caracteristicas, imagen, precio, gramaje, variedad_id, idMod]);
+        const { idMod, nombre, caracteristicas, imagen, precio, gramaje, variedad_id, imagenActual } = req.body;
+        const img = imagen == "" ? imagenActual : imagen;
+        const modificado = await conn.query(sql, [nombre, caracteristicas, img, precio, gramaje, variedad_id, idMod]);
         res.redirect('/producto');
     },
 
